c622d0d6d8d17626b83a86e54a258a99fb97fa59
MrTavis <taviskaaa@gmail.com>
2026-09-11 23:24:06 +0400

    Import scrollBy extension in wear MainActivity

--- wear/src/main/java/com/watchbot/mathsync/wear/presentation/MainActivity.kt
+++ wear/src/main/java/com/watchbot/mathsync/wear/presentation/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
